2052640d03672990cab8cdcd1a9cda76e4d9cb62
Daniel Weber <d.weber@micromata.de>
2021-02-01 21:10:05 +0100

    Rewards were now persistent

--- src/Reward.js
+++ src/Reward.js
@@ -6,24 +6,31 @@ import Dog from "./Dog";
 import Giraffe from "./Giraffe";
 import GiraffeItem from "./lemmling-Cartoon-giraffe.svg";
 import {Badge, ProgressBar} from "react-bootstrap";
+import {useLocalStorage} from "./useLocalStorage";
 
 function Reward(props) {
     const [gangster, setGlasses] = useState(false);
     const [smile, setMouth] = useState(false);
     const [giraffe, setGiraffe] = useState(false);
 
+    const [rewards, setRewards] = useLocalStorage('rewards',
+        {
+            gangster: false,
+            smile: false,
+            giraffe: false
+        });
+
     return (
         <div className="reward">
 
-            {/*<img src={lemming} className="splashImage"/>*/}
-            <Dog smile={smile} gangster={gangster}/>
-            {giraffe && <Giraffe/>}
+            <Dog smile={rewards.smile} gangster={rewards.gangster}/>
+            {rewards.giraffe && <Giraffe/>}
             <div style={{flexBasis: "100%", height: 0}}></div>
             <div>
                 <div className="rewardItems">
-                    <img className="rewardItem" src={glasses} onClick={() => setGlasses(!gangster)}/>
-                    <img className="rewardItem" src={mouth} onClick={() => setMouth(!smile)}/>
-                    <img className="rewardItem" src={GiraffeItem} onClick={() => setGiraffe(!giraffe)}/>
+                    <img className="rewardItem" src={glasses} onClick={() => setRewards({...rewards, gangster: !rewards.gangster})}/>
+                    <img className="rewardItem" src={mouth} onClick={() => setRewards({...rewards, smile: !rewards.smile})}/>
+                    <img className="rewardItem" src={GiraffeItem} onClick={() => setRewards({...rewards, giraffe: !rewards.giraffe})}/>
                 </div>
 
             </div>
